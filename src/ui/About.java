1a61d836577c0508c3e1547e689df04a6a7017cd
180138Ga <grace.m.arnold@outlook.com>
2020-04-24 17:09:48 -0500

    Fixing about????

--- src/ui/About.java
+++ src/ui/About.java
@@ -48,6 +48,7 @@ public class About {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
@@ -55,9 +56,11 @@ public class About {
 		lblAbout.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAbout.setVerticalAlignment(SwingConstants.TOP);
 		
+		JTextArea textArea = new JTextArea();
+		
 		JTextArea txtrParkingRUs = new JTextArea();
+		txtrParkingRUs.setEditable(false);
 		txtrParkingRUs.setLineWrap(true);
-		txtrParkingRUs.setRows(5);
 		txtrParkingRUs.setText("Parking R Us is a parking web application created by Grace Arnold, Christopher Dharma, and Gladys Toledo-Rodriguez. With it, you can reserve a parking spot at a parking lot by providing an address, date, time, and the license plate of the car to be parked. You can also purchase a monthly subscription for a given time for easier convenience. Sign up today!");
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
@@ -68,17 +71,26 @@ public class About {
 							.addGap(110)
 							.addComponent(lblAbout, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE))
 						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(39)
+							.addComponent(textArea, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(txtrParkingRUs, GroupLayout.DEFAULT_SIZE, 398, Short.MAX_VALUE)))
+							.addComponent(txtrParkingRUs, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblAbout)
-					.addPreferredGap(ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
-					.addComponent(txtrParkingRUs, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblAbout)
+							.addGap(60))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(textArea, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(58)))
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(txtrParkingRUs, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		frame.getContentPane().setLayout(groupLayout);
