39500ac4092109a81ad7b504d39bbd8bcb2fa0b4
180138Ga <grace.m.arnold@outlook.com>
2020-04-24 15:30:37 -0500

    made welcome page

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -15,6 +15,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import net.miginfocom.swing.MigLayout;
 
 class GUI {
 	private static JFrame frame; // main java swing frame
@@ -27,12 +33,16 @@ class GUI {
 
 	// footer object declarations
 	private static JPanel optionsPanel;
-	private static JButton randomizeNumbersBtn; // create new random array of ints
-	private static JButton sortBtn; // initiate sorting process
 
 	private static int[] randData; // stores current array to be sorted
 	private static JButton btnParkingRUs;
 	private static JButton btnAbout;
+	private static JLabel lblMadeByGrace;
+	private static JLabel lblWelcome;
+	private static JButton btnNewButton;
+	private static JButton btnSignUp;
+	private static JLabel label;
+	private static JLabel label_1;
 
 	public static void initializeGUI() {
 		// Initialize GUI
@@ -55,30 +65,41 @@ class GUI {
 		// Creating menu panel at bottom
 		optionsPanel = new JPanel();
 
-		// Generate new set of random numbers
-		randomizeNumbersBtn = new JButton("Randomize Numbers");
-		randomizeNumbersBtn.setActionCommand("Randomize");
-		randomizeNumbersBtn.addActionListener(new ButtonClickListener());
-
-		// Start sorting array
-		sortBtn = new JButton("Sort!");
-		sortBtn.setActionCommand("Sort");
-		sortBtn.addActionListener(new ButtonClickListener());
-
-		// Add buttons to options panel
-		optionsPanel.add(randomizeNumbersBtn);
-		optionsPanel.add(sortBtn);
-
 		// align and add panes to frame
 		frame.getContentPane().add(BorderLayout.NORTH, headerPanel);
+		headerPanel.setLayout(new MigLayout("", "[123px][75px]", "[29px][]"));
 		
 		btnParkingRUs = new JButton("Parking R Us");
-		headerPanel.add(btnParkingRUs);
+		headerPanel.add(btnParkingRUs, "cell 0 0,alignx left,aligny top");
 		
 		btnAbout = new JButton("About");
-		headerPanel.add(btnAbout);
+		headerPanel.add(btnAbout, "cell 0 1,alignx left,aligny top");
 		frame.getContentPane().add(BorderLayout.SOUTH, optionsPanel);
+		
+		lblMadeByGrace = new JLabel("Made by Grace Arnold, Chris Dharma, and Gladys Toledo-Rodriguez");
+		optionsPanel.add(lblMadeByGrace);
 		frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
+		centerPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		lblWelcome = new JLabel("Welcome!");
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		centerPanel.add(lblWelcome);
+		
+		btnNewButton = new JButton("Log In");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		label = new JLabel("");
+		centerPanel.add(label);
+		
+		label_1 = new JLabel("");
+		centerPanel.add(label_1);
+		centerPanel.add(btnNewButton);
+		
+		btnSignUp = new JButton("Sign Up");
+		centerPanel.add(btnSignUp);
 		// turn on frame
 		frame.setVisible(true);
 	}
@@ -86,91 +107,5 @@ class GUI {
 	public static void main(String[] args) {
 		// calls initializeGUI to initialize the window and components
 		initializeGUI();
-	}
-
-	/*
-	 * ButtonClickListener is a class that allows for event handlers to be captured
-	 * from the functional buttons. This class is designed to be used with the
-	 * JFrame and JTextArea in the main class, thus needs the references to those
-	 * objects
-	 */
-	private static class ButtonClickListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				String command = e.getActionCommand();
-				// Switch on command passed
-				switch (command) {
-				// Generate a new array to sort for both algorithms
-				case "Randomize":
-					System.out.println("Randomize button pressed");
-					// Display message asking user how many elements they want
-					String result = (String) JOptionPane.showInputDialog(frame,
-							"How many random numbers would you like?", null, JOptionPane.PLAIN_MESSAGE, null, null,
-							null);
-					// Check if they pressed cancel on the dialog box
-					if (result != null) {
-						// if result is able to be parsed, then parse
-						int resultNum = Integer.parseInt(result);
-						if (resultNum > 0 && resultNum < 101) {
-							// create new array of size resultNum
-							randData = new int[7];
-							randData[0] = 21;
-							randData[1] = 345;
-							randData[2] = 13;
-							randData[3] = 101;
-							randData[4] = 50;
-							randData[5] = 234;
-							randData[6] = 1;
-							// populate array with random numbers
-							//for (int i = 0; i < randData.length; i++) {
-								//randData[i] = (int) (Math.random() * randData.length);
-								
-							//}
-							// display message indicating array has be created
-							JOptionPane.showMessageDialog(frame, "New array generated!\n");
-							// update currentArray label with correct formatting
-						} else {
-							// number too small or too big
-							JOptionPane.showMessageDialog(frame,
-									"Please choose a number greater than 0 and less than 101!");
-						}
-					}
-					// Otherwise, they cancelled, and nothing happens
-					break;
-				case "Sort":
-					// Sort button has been pressed, thus the algorithms will
-					// sort the current array
-					System.out.println("Sort button pressed");
-					// clears text areas for new text
-
-					// counting sort called
-					// starts timing counting sort
-					long startTime = System.nanoTime();
-					// sleeps to align timer
-					TimeUnit.SECONDS.sleep(1);
-					//countingSort(Arrays.copyOf(randData, randData.length));
-					// stops timer
-					long endTime = System.nanoTime();
-					// calculates elapsed time for counting sort to finish
-
-					// radix sort called
-					// starts new timer
-					startTime = System.nanoTime();
-					// new timer alignment
-					TimeUnit.SECONDS.sleep(1);
-					// stops timer
-					endTime = System.nanoTime();
-					// calculates elapsed time for radix sort to finish
-					break;
-				}
-			} catch (NumberFormatException nfe) {
-				// Catch the user not inputting a number in the dialog box
-				JOptionPane.showMessageDialog(null, "Please enter a valid number!");
-			} catch (InterruptedException ie) {
-				// Catch interrupted execution from sleep function
-				System.err.print("Interrupted during execution!");
-			}
-		}
 	}
 }
