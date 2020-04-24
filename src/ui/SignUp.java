f41ac139df8630fbea9ebd39ec19d7c093362b70
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2020-04-24 16:25:42 -0500

    Finished separating into panels

--- src/ui/SignUp.java
+++ src/ui/SignUp.java
@@ -1,13 +1,32 @@
 package ui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import net.miginfocom.swing.MigLayout;
 
 public class SignUp {
 
 	private JFrame frame;
-
+	// header object declarations
+		private static JPanel headerPanel; // panel for header
+		// footer object declarations
+		private static JPanel optionsPanel;
+		
+		private static JButton btnParkingRUs;
+		private static JButton btnAbout;
+		private JLabel lblMadeByGladys;
+		
 	/**
 	 * Launch the application.
 	 */
@@ -35,9 +54,40 @@ public class SignUp {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		// Initialize GUI
+		frame = new JFrame("Parking R Us");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationByPlatform(true);
+		frame.setSize(900, 500);
+		frame.setResizable(false);
+		System.out.println("GUI Initialized!"); // debug
+
+		// Create panel for top
+		headerPanel = new JPanel();
+
+		// Creating menu panel at bottom
+		optionsPanel = new JPanel();
+
+		// align and add panes to frame
+		frame.getContentPane().add(BorderLayout.NORTH, headerPanel);
+		headerPanel.setLayout(new MigLayout("", "[123px][75px]", "[29px][]"));
+		
+		btnParkingRUs = new JButton("Parking R Us");
+		headerPanel.add(btnParkingRUs, "cell 0 0,alignx left,aligny top");
+		
+		btnAbout = new JButton("About");
+		btnAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				About about = new About();
+				about.NewScreen();
+			}
+		});
+		headerPanel.add(btnAbout, "cell 0 1,alignx left,aligny top");
+		frame.getContentPane().add(BorderLayout.SOUTH, optionsPanel);
+		
+		lblMadeByGladys = new JLabel("Made by Grace Arnold, Chris Dharma, and Gladys Toledo-Rodriguez");
+		optionsPanel.add(lblMadeByGladys);
+		
 	}
 	public void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
